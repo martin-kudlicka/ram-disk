2a6430a279794d2c913bcd87a292d2119bbc886a
Martin Kudlička <email@martink.cz>
2020-06-27 17:44:40 +0000

    + basic disk dialog

---
+++ source/diskdialog.cpp
@@ -0,0 +1,39 @@
+#include "pch.h"
+#include "diskdialog.h"
+
+DiskDialog::DiskDialog(QWidget *parent) : DiskDialog(MUuidPtr::createUuid(), parent)
+{
+}
+
+DiskDialog::DiskDialog(const MUuidPtr &id, QWidget *parent) : QDialog(parent), _widgetSettings(&_options)
+{
+  Q_UNUSED(id);
+
+  _ui.setupUi(this);
+
+  setupWidgets();
+  setupSettings();
+}
+
+void DiskDialog::setupSettings()
+{
+  _widgetSettings.setWidget(DiskOptions::Property::Size,   _ui.size);
+  _widgetSettings.setWidget(DiskOptions::Property::Letter, _ui.letter);
+
+  _widgetSettings.load();
+}
+
+void DiskDialog::setupWidgets()
+{
+  for (auto letter = 'A'; letter <= 'Z'; ++letter)
+  {
+    _ui.letter->addItem(QString(letter));
+  }
+}
+
+void DiskDialog::accept()
+{
+  _widgetSettings.save();
+
+  QDialog::accept();
+}
