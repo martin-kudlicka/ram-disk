a1a138527e4d27f178f0349237c206d671108b29
Martin Kudlička <email@martink.cz>
2021-04-05 14:14:32 +0000

    + format ram disk with FAT32 filesystem

--- storages/storage-winspd/ramdisk-winspd.cpp
+++ storages/storage-winspd/ramdisk-winspd.cpp
@@ -24,7 +24,35 @@ void RamDiskWinSpd::start()
 
   try
   {
+    auto volumesBefore = MVolumes::enumerate();
+
     _bufferDevice->open(QIODevice::ReadWrite);
+
+    QStringList volumesAfter;
+    for (auto run = 0; run < 10; ++run)
+    {
+      Sleep(100);
+
+      volumesAfter = MVolumes::enumerate();
+      if (volumesAfter.count() > volumesBefore.count())
+      {
+        break;
+      }
+    }
+
+    if (volumesBefore.count() + 1 != volumesAfter.count())
+    {
+      throw MException::MCritical(WinSpd(), ERROR_MOUNT_POINT_NOT_RESOLVED, "can't find created ram disk volume");
+    }
+
+    auto newVolume = std::find_if_not(volumesAfter.cbegin(), volumesAfter.cend(), [&volumesBefore](const QString &volumeName)
+    {
+      return volumesBefore.contains(volumeName);
+    });
+
+    MStorage::format(*newVolume, MVolume::FileSystem::FAT32, QCoreApplication::applicationName(), false);
+
+    // TODO assign letter
   }
   catch (const MException::MCritical &)
   {
